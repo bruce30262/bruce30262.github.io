dd0f59ed45a8c8299632637fea32348f7a85217b
Bruce Chen <bruce30262@pm.me>
2021-07-19 21:45:38 +0800

    Update site

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210719.2128';
+const cacheName = 'chirpy-20210719.2144';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
